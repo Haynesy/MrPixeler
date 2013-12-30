56f01301d6e4f53282449a5ae4ba78aae86e7ee4
Adam Haynes <haynesy.adam@gmail.com>
2013-12-30 15:17:44 +1100

    Add 3D functionality

--- js/app.js
+++ js/app.js
@@ -71,3 +71,71 @@ $(document).ready(function(){
 
 console.clear();
 console.log('Loaded');
+
+
+
+//-------------------------------
+// Create 3D
+//-------------------------------
+
+function Initialize3D(){
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+
+	// Create utilities
+	var clock = new THREE.Clock;
+
+	// Set up the Geometry
+
+		// Textures
+		var cubeTexture = THREE.ImageUtils.loadTexture("textures/box.png");
+
+		// Cube
+		var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
+		var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 }); //map: cubeTexture
+		var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+		cube.rotation.y = Math.PI * 45 / 180;
+
+		// Skybox
+		var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+		// var cubeMaterial = new THREE.MeshLambertMaterial({ map: cubeTexture, color: 0x28c0ec });
+		var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide });
+		var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+
+	// Create the camera
+	var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+	camera.position.y = 160;
+	camera.position.z = 400;
+	camera.lookAt(cube.position);
+
+	// Create the scene
+		// Point Light
+		var pointLight = new THREE.PointLight(0xffffff);
+		pointLight.position.set(0, 300, 200);
+
+		// Camera
+		var scene = new THREE.Scene;
+		scene.add(cube); 
+		scene.add(camera);
+		scene.add(skybox);
+		scene.add(pointLight);
+
+	// Create the renderer
+	var renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+	 
+	render();
+
+}
+
+function render() {
+   renderer.render(scene, camera);
+    
+   // Do scene render
+   cube.rotation.y -= clock.getDelta();
+
+   requestAnimationFrame(render);
+}
+
+Initialize3D();
